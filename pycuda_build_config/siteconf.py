7f270b1369b5f63153aad80163cfeac2eca9e6c4
Guru-Deep-Singh <gurudeep1998@gmail.com>
2025-11-20 18:28:19 +0100

    feat(): siteconfig for the pycuda

---
+++ pycuda_build_config/siteconf.py
@@ -0,0 +1,7 @@
+# Set the necessary paths for Jetson Nano (JetPack 6.x)
+CUDADRV_LIB_DIR = ['/usr/lib/aarch64-linux-gnu'] 
+
+BOOST_INC_DIR = ['/usr/include']
+BOOST_LIB_DIR = ['/usr/lib/aarch64-linux-gnu']
+BOOST_PYTHON_LIBNAME = ['boost_python3']
+
